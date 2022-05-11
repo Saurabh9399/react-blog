0c0852eea9f59f3a0c8f89b34313316bb5f209d8
saurabh <saurabhj8087445@gmail.com>
2022-05-11 23:59:20 +0530

    Adding routing files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
-
+import { Routes, Route } from "react-router-dom";
+import CreatePost from "./CreatePost";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import PostDetail from "./PostDetail";
 
 function App() {
   return (
     <div className="App">
-      Starting React blog
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/post/:postId" element={<PostDetail/>} />
+        <Route exact path="/create-post" element={<CreatePost/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import App from './App';
+import Navbar from './Navbar';
+import Home from './Home';
+import PostDetail from './PostDetail';
+import CreatePost from './CreatePost';
+
+
+export {
+    App,
+    Navbar,
+    CreatePost,
+    Home,
+    PostDetail
+}
